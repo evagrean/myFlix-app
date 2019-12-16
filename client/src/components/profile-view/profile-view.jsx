951ae87ba87fef5852e9c25857ab8ea27cd2de9a
evagrean <someone@someplace.com>
2019-12-16 12:37:27 +0100

    worked on layout and list of favorites

--- client/src/components/profile-view/profile-view.jsx
+++ client/src/components/profile-view/profile-view.jsx
@@ -1,41 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import { Form } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 
-export class ProfileView extends React.Component {
+import './profile-view.scss';
 
-  constructor() {
-    super();
-    this.state = {
-      username: null,
-      password: null,
-      email: null,
-      birthday: null,
-      userData: null,
-      favorites: []
-    };
-  }
+export function ProfileView(props) {
+  const { username, password, email, birthday, favorites, movies } = props;
 
-  componentDidMount() {
-    //authentication
-    let accessToken = localStorage.getItem('token');
-    if (accessToken !== null) {
-      this.getUser(accessToken);
-    }
-  }
 
-  render() {
-    const { username } = this.state;
-    return (
-      <div className="profile-view">
-        <h1 className="display-4">Profile</h1>
-        <Row className="justify-content-center">
-          <Col xs={10} sm={6} md={4}>
+
+  console.log(props);
+  console.log(favorites);
+
+  return (
+    <div className="profile-view">
+      <Container>
+        <Card style={{ minwidth: '20rem' }} className="border-0 pl-0">
+          <Card.Body>
+            <span className="d-flex align-items-center mb-4">
+              <Link to={`/`}>
+                <i className="material-icons">arrow_back_ios</i>
+              </Link>
+              <h1 className="display-4">Profile</h1>
+            </span>
+            <Card.Text className="mb-4 lead">
+              Username: {username} <br />
+              Email: {email} <br />
+              Birthday: {birthday.slice(0, 10)} <br />
+            </Card.Text>
+            <Link to={`/update/${username}`}>
+              <Button variant="primary" className="update-button">Update my profile</Button>
+            </Link>
+
+            <Button variant="primary" className="delete-button ml-2">Delete my profile</Button>
+          </Card.Body>
+        </Card>
+        <Container>
+          <h4 className="mt-4">Your favorite movies: </h4>
+          {favorites.length === 0 &&
+            <div>You have no favorite movies</div>}
+          {favorites.length > 0 &&
+            <div>
+              {favorites}
+            </div>
+          }
+
+        </Container>
+      </Container>
+    </div >
+
+
+
+
+
+  );
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Form to insert later when everything runs as expected
+// ---------------------------------------------------------------
+/* <Row className="justify-content-center">
+          <Col>
             <Container className="container register-container border border-light rounded py-3 px-3">
               <Form className="registration-form">
                 <Form.Group controlId="formBasicUsername">
@@ -67,14 +116,6 @@ export class ProfileView extends React.Component {
             </Container>
           </Col>
         </Row>
-        <h3>My favorite movies</h3>
-      </div >
-
-    );
-  }
-
-}
-
-
+        <h3>My favorite movies</h3> */
 
 
