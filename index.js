04ccb33606fd5706d4d445d60af9717b2303b05f
evagrean <someone@someplace.com>
2019-12-20 21:47:29 +0100

    toggled mongoose connection

--- index.js
+++ index.js
@@ -13,20 +13,20 @@ const { check, validationResult } = require('express-validator');
 const app = express();
 
 // Allowing Mongoose to connect to online database on MongoDB Atlas
-mongoose.connect(
-  'mongodb+srv://myDBadmin:pWTbPxTFC6Lr3Mbk@mydb-vyikg.mongodb.net/myFlixDB?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
+// mongoose.connect(
+//   'mongodb+srv://myDBadmin:pWTbPxTFC6Lr3Mbk@mydb-vyikg.mongodb.net/myFlixDB?retryWrites=true&w=majority',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   }
+// );
 
-/* Allowing Mongoose to connect to local database
+// Allowing Mongoose to connect to local database
 mongoose.connect('mongodb://localhost:27017/myFlixDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-*/
+
 
 mongoose.set('useFindAndModify', false);
 
@@ -170,7 +170,7 @@ Birthday: Date
 app.post(
   '/users',
   [
-    check('Username', 'Username is required').isLength({ min: 5 }),
+    check('Username', 'Username has to be at least five characters long').isLength({ min: 5 }),
     check(
       'Username',
       'Username contains non alphanumeric characters - not allowed.'
