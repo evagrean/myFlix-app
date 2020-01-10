0f9d384183dfeb03f47491423b4b4a033a6312c2
evagrean <someone@someplace.com>
2020-01-10 13:57:11 +0100

    added VisibilityFilterInput component

---
+++ client/src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import Form from 'react-bootstrap/Form';
+
+import { setFilter } from '../../actions/actions';
+
+import './visibility-filter-input.scss';
+
+function VisibilityFilterInput(props) {
+  return <div className="visibility-filter-input">
+    <Form.Group controlId="formBasicFilterInput" className="mb-4">
+      <Form.Control className="search" onChange={e => props.setFilter(e.target.value)} value={props.visibilityFilter.trim().toLowerCase()} placeholder="Search..." />
+    </Form.Group>
+
+  </div>
+
+}
+
+export default connect(null, { setFilter })(VisibilityFilterInput);
+
+
+
