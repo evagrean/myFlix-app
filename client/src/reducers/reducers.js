0b6938907c0a8161e685c6d4a3a68883a594ee9c
evagrean <someone@someplace.com>
2020-01-14 11:14:27 +0100

    removed console.log() after testing

--- client/src/reducers/reducers.js
+++ client/src/reducers/reducers.js
@@ -2,6 +2,9 @@ import { combineReducers } from 'redux';
 
 import { SET_MOVIES, SET_FILTER, SET_USER_PROFILE } from '../actions/actions';
 
+// reducer takes current state and an action as arguments and figures out the new state. 
+// state argument is assigned to a initial state at the beginning.
+
 function visibilityFilter(state = '', action) {
   switch (action.type) {
     case SET_FILTER:
